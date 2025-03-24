6cbf6f87aafb3af3b9d15e3f444bcec23bec218c
David Howe <65414349+bagodrago@users.noreply.github.com>
2025-03-24 18:16:31 -0400

    Created skeleton code for TransactionHandler class

---
+++ TransactionHandler.py
@@ -0,0 +1,58 @@
+from Toolbox import Toolbox
+
+class TransactionHandler:
+    """
+    Handles the application of transactions to account data
+    """
+    # TODO: Ensure all transactions implement Toolbox.log_constraint_error()
+
+    @staticmethod
+    def withdraw(account, transaction):
+        # TODO: Implement transaction to apply changes to given account
+        pass
+
+    @staticmethod
+    def transfer(account, transaction):
+        # TODO: Implement transaction to apply changes to given account
+        pass
+
+    @staticmethod
+    def paybill(account, transaction):
+        # TODO: Implement transaction to apply changes to given account
+        pass
+
+    @staticmethod
+    def deposit(account, transaction):
+        # TODO: Implement transaction to apply changes to given account
+        pass
+
+    @staticmethod
+    def create(account, transaction):
+        # TODO: Implement transaction to apply changes to given account
+        pass
+
+    @staticmethod
+    def delete(account, transaction):
+        # TODO: Implement transaction to apply changes to given account
+        pass
+
+    @staticmethod
+    def disable(account, transaction):
+        # TODO: Implement transaction to apply changes to given account
+        pass
+
+    @staticmethod
+    def changeplan(account, transaction):
+        # TODO: Implement transaction to apply changes to given account
+        pass
+
+
+    @staticmethod
+    def apply(accounts, transactions):
+        """
+        Applies list of transactions to given account list
+        """
+        # TODO: Iterate through transactions
+        # TODO: Search for corresponding account (Hint: Use Toolbox.search_account())
+        # TODO: Apply transaction to account (Hint: Use Toolbox.decode_tc())
+        pass
